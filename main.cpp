833ad1b1474e785fc9fafc67923806926e5d5d8a
iceman2146 <30576571+iceman2146@users.noreply.github.com>
2022-11-06 12:48:51 +0300

    Сделал папочки, пока хочу вставить имгуи

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 
+
 using namespace cv;
 void createAlphaImage(const cv::Mat_<cv::Vec4b>& mat, cv::Mat_<cv::Vec4b> &dst)
 {
@@ -14,10 +15,10 @@ void createAlphaImage(const cv::Mat_<cv::Vec4b>& mat, cv::Mat_<cv::Vec4b> &dst)
 int main(int argc, char** argv )
 {
 
-    Mat_<cv::Vec4b> image_RGB;
-    Mat_<cv::Vec4b> image_A;
-    image_RGB = imread("C:/Users/icema/Desktop/OTUS_Project/lenna.png");
-    image_A = imread("C:/Users/icema/Desktop/OTUS_Project/alpha.png");
+    cv::Mat_<cv::Vec4b> image_RGB;
+    cv::Mat_<cv::Vec4b> image_A;
+    image_RGB = imread("C:/Users/icema/Desktop/OTUS_Project/Imported_Resource/lenna.png");
+    image_A = imread("C:/Users/icema/Desktop/OTUS_Project/Imported_Resource/alpha.png");
     if ( !image_RGB.data||!image_A.data )
     {
         printf("No image data \n");
@@ -25,9 +26,18 @@ int main(int argc, char** argv )
     }
     //Mat temp;
     //cv::cvtColor(image_RGB,temp,COLOR_BGR2BGRA,4);
-    createAlphaImage(image_A,image_RGB);
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image_A);
+    //createAlphaImage(image_A,image_RGB);
+    namedWindow("Изображение", WINDOW_AUTOSIZE );
+    imshow("Изображение", image_RGB);
+    /*cv::Mat_<cv::Vec4b> imgs;
+    imgs.push_back(image_RGB);
+    imgs.push_back(~image_RGB);
+    imgs.push_back(image_RGB(Rect(0, 0, image_RGB.cols / 2, image_RGB.rows / 2)));
+    imwrite("test.png", imgs);
+    
+    imwrite("test.png", image_RGB);*/
     waitKey(0);
+    
+    
     return 0;
 }
